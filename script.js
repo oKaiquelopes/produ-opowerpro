4d03debf2d189c9dec2778d59bd0e924afa08b2e
Kaique Lopes <91297462+oKaiquelopes@users.noreply.github.com>
2024-01-14 14:35:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "css/imagens/icons/menu-abertoPreto.png";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "css/imagens/icons/botao-fecharPreto.png";
+    }
+}
+
+function toggleDropdown() {
+    var dropdownContent = document.getElementById("myDropdown");
+    dropdownContent.classList.toggle("show");
+}
+  // Fechar o menu dropdown se o usuário clicar fora dele
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+};
